8b968c5302c507b1486c4fc21e4339a63bcbd433
unknown <artkwi@st.amu.edu.pl>
2018-12-27 18:29:25 +0100

    emoji-mart added to message field

--- chatapp/src/components/Chat.js
+++ chatapp/src/components/Chat.js
@@ -1,15 +1,20 @@
 import React from "react";
 import io from "socket.io-client";
+import 'emoji-mart/css/emoji-mart.css';
+import { Picker } from 'emoji-mart';
+
 
 class Chat extends React.Component {
     constructor(props) {
         super(props);
+        this.handleChange = this.handleChange.bind(this);
 
         this.state = {
             username: '',
             message: '',
             messages: []
         };
+
 
         this.socket = io('localhost:8080');
 
@@ -19,19 +24,43 @@ class Chat extends React.Component {
                 author: this.state.username,
                 message: this.state.message
             });
-            this.setState({message: ''});
+            this.setState({ message: '' });
         };
 
-        this.socket.on('RECEIVE_MESSAGE', function(data){
+        this.socket.on('RECEIVE_MESSAGE', function (data) {
             addMessage(data);
         });
-        
+
         const addMessage = data => {
             console.log(data);
-            this.setState({messages: [...this.state.messages, data]});
+            this.setState({ messages: [...this.state.messages, data] });
             console.log(this.state.messages);
         };
     }
+
+    handleChange(event) {
+        this.setState({ message: event.target.value });
+    }
+
+    addEmoji = (e) => {
+        console.log(e)
+        if (e.unified.length <= 5){
+          let emojiPic = String.fromCodePoint(`0x${e.unified}`)
+          this.setState({
+            message: this.state.message + emojiPic
+          })
+        }else {
+          let sym = e.unified.split('-')
+          let codesArray = []
+          sym.forEach(el => codesArray.push('0x' + el))
+          //console.log(codesArray.length)
+          //console.log(codesArray)  // ["0x1f3f3", "0xfe0f"]
+          let emojiPic = String.fromCodePoint(...codesArray)
+          this.setState({
+            message: this.state.message + emojiPic
+          })
+        }
+      }
 
     render() {
         return (
@@ -40,19 +69,20 @@ class Chat extends React.Component {
                     <div className="col-4">
                         <div className="card">
                             <div className="card-body">
+                                <Picker onSelect={this.addEmoji} />
                                 <div className="card-title">Global Chat</div>
                                 <hr />
                                 <div className="messages">
                                     {this.state.messages.map(message => {
-                                    return (
-                                    <div>{message.author}: {message.message}</div>
-                                    )
+                                        return (
+                                            <div>{message.author}: {message.message}</div>
+                                        )
                                     })}
                                 </div>
                                 <div className="footer">
                                     <input type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({ username: ev.target.value })} className="form-control" />
                                     <br />
-                                    <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({ message: ev.target.value })} />
+                                    <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={this.handleChange} />
                                     <br />
                                     <button onClick={this.sendMessage} className="btn btn-primary form-control">Send</button>
                                 </div>
